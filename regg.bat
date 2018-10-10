82c91ac4a7ac1b068ae1096fc5f36b64e6924879
delarmgo9 <44018442+delarmgo9@users.noreply.github.com>
2018-10-10 09:44:36 +0200

    Update regg.bat

--- regg.bat
+++ regg.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM -----------------------------------------
-REM BACKUP DE TOUTE LA BASE DE REGISTRE : (*.reg dans le dossier REGBACKUP sur le Bureau de l'Utilisateur loggué)
+REM BACKUP DE TOUTE LA BASE DE REGISTRE : (*.reg dans le dossier REGBACKUP sur le Bureau de l'Utilisateur logguÃ©)
 
 IF NOT EXIST %USERPROFILE%\Desktop\REGBACKUP GOTO MAKEDIR
 RMDIR /Q /S %USERPROFILE%\Desktop\REGBACKUP
@@ -12,36 +12,36 @@ FOR %%K IN (LM CU CR U CC) DO @REG.EXE EXPORT HK%%K %USERPROFILE%\Desktop\REGBAC
 
 
 REM -----------------------------------------
-REM RECHERCHE DE LA VALEUR "merde" DANS LA CLEF "Couleur" :
+REM RECHERCHE DE LA VALEUR "tototest" DANS LA CLEF "Couleur" :
 
 set Coul=1
-for /f "tokens=4" %%a in ('reg query "HKLM\SOFTWARE\Wow6432Node\ABL Informatique\M2G\Impression\BLCompas" /V Couleur ^|findstr /sli "REG_SZ"') do set Coul=%%a
+for /f "tokens=4" %%a in ('reg query "HKLM\SOFTWARE\Wow6432Node\ABC\QSD\Impression\BLCo" /V Couleur ^|findstr /sli "REG_SZ"') do set Coul=%%a
 
 
 REM -----------------------------------------
-REM SI VALEUR "merde" PRESENTE GOTO CHANGE_VAL :
+REM SI VALEUR "tototest" PRESENTE GOTO CHANGE_VAL :
 
-if "%Coul%"=="merde" GOTO CHANGE_VAL
+if "%Coul%"=="tototest" GOTO CHANGE_VAL
 REM SINON
 GOTO ENDD1
 
 
 REM -----------------------------------------
-REM REMPLACE "Bleu merde" PAR "Bleu" :
+REM REMPLACE "Bleu tototest" PAR "Bleu" :
 
 :CHANGE_VAL
-REG ADD "HKLM\SOFTWARE\Wow6432Node\ABL Informatique\M2G\Impression\BLCompas" /f /v Couleur /t REG_SZ /d bleu
-ECHO La Valeur recherch‚e a bien ‚t‚ remplac‚e.
+REG ADD "HKLM\SOFTWARE\Wow6432Node\ABC\QSD\Impression\BLCo" /f /v Couleur /t REG_SZ /d bleu
+ECHO La Valeur recherchâ€še a bien â€štâ€š remplacâ€še.
 
 GOTO ENDD2
 
 
 REM -----------------------------------------
-REM SI VALEUR "merde" NON PRESENTE :
+REM SI VALEUR "tototest" NON PRESENTE :
 
 :ENDD1
 
-ECHO La Valeur recherch‚e n'a pas ‚t‚ trouv‚e.
+ECHO La Valeur recherchâ€še n'a pas â€štâ€š trouvâ€še.
 
 
 REM -----------------------------------------
